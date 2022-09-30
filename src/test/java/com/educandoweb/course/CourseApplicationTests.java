b5d10cbd103dd91a6d33e58c5d6b33d78e41ced1
Nelio Alves <acenelio@gmail.com>
2022-09-30 11:17:12 -0300

    Project created

---
+++ src/test/java/com/educandoweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
